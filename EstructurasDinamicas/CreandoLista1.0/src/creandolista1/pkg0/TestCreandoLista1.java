db23dbe1f1ba86238e6ae888ea87b1a99ec79e02
andresWeitzel <58787260+andresWeitzel@users.noreply.github.com>
2020-05-10 20:18:22 -0300

    Add files via upload

--- EstructurasDinamicas/CreandoLista1.0/src/creandolista1/pkg0/TestCreandoLista1.java
+++ EstructurasDinamicas/CreandoLista1.0/src/creandolista1/pkg0/TestCreandoLista1.java
@@ -16,6 +16,33 @@ public class TestCreandoLista1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("\n♦♦CREANDO NUESTRA LISTA DINAMICA♦♦");
+        
+        ListaDinamica<String> lista=new ListaDinamica<String>();
+        
+        System.out.println("Agregando elementos...");
+        lista.add("Alvear", 0);
+        lista.add("Martinez", 1);
+        lista.add("Rodriguez",2);
+        lista.add("Ivromich",3);
+        lista.addLast("De Richetti");
+        lista.addFirst("Extromitji");
+        lista.addFirst("Puntijuitkki");
+        
+        System.out.println("\n--La lista esta vacia?--");
+        System.out.println(lista.isEmpty());
+        
+        System.out.println("\n--Longitud de la lista?--");
+        System.out.println(lista.size());
+        
+        System.out.println("\n--Cual es el primer elemento de nuetra lista?--");
+         System.out.println(lista.getFirst());
+         
+          System.out.println("\n--Cual es el ultimo elemento de nuetra lista?--");
+         System.out.println(lista.getLast());
+         
+        System.out.println("\n--Imprimimos nuestra Lista completa de menor a mayor indice--");
+        System.out.println(lista.toString());
         
         
     }
